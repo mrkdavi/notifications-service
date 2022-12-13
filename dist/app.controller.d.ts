067afa38a4ed4bda003809015772e7b5df7e8e78
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-12-13 13:05:04 -0300

    Remove dist

--- dist/app.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from './prisma.service';
-import { createNotificationBody } from './create-notification-body';
-export declare class AppController {
-    private readonly prismaService;
-    constructor(prismaService: PrismaService);
-    list(): import(".prisma/client").PrismaPromise<import(".prisma/client").Notification[]>;
-    create(body: createNotificationBody): Promise<import(".prisma/client").Notification[]>;
-}
--- dist/create-notification-body.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class createNotificationBody {
-    recipientId: string;
-    content: string;
-    category: string;
-}
